4ba759f385a97c446f5eb600e67311c501a4aba7
Gianfranco Iacono <iacono.gian13@gmail.com>
2026-03-31 20:41:06 -0300

    feat(store): create weather state managment with zustand

---
+++ src/features/weather/store/weather.store.ts
@@ -0,0 +1,35 @@
+import { create } from 'zustand';
+
+import type { CurrentWeather } from '../../../types/weather.types';
+
+interface WeatherState {
+  latitude: number | null;
+  longitude: number | null;
+  currentWeather: CurrentWeather | null; 
+  isLoading: boolean;
+  error: string | null;
+}
+
+interface WeatherActions {
+  setLocation: (latitude: number, longitude: number) => void;
+  setCurrentWeather: (weatherData: CurrentWeather | null) => void;
+  setLoading: (isLoading: boolean) => void;
+  setError: (error: string | null) => void;
+}
+
+
+
+export const useWeatherStore = create<WeatherState & WeatherActions>((set) => ({
+  latitude: null,
+  longitude: null,
+  currentWeather: null,
+  isLoading: false,
+  error: null,
+
+
+  setLocation: (latitude, longitude) => set({ latitude, longitude }),
+  setCurrentWeather: (weatherData) => set({ currentWeather: weatherData }),
+  setLoading: (isLoading) => set({ isLoading }),
+  setError: (error) => set({ error }),
+
+}));
