1df946919ddb4cfd9112f8aa9483fa18da53162c
Gianfranco Iacono <iacono.gian13@gmail.com>
2026-04-24 13:35:35 -0300

    refactor(weather): remove unused error and no data components from WeatherDetailsSection

--- src/features/weather/sections/WeatherDetailsSection/components/index.ts
+++ src/features/weather/sections/WeatherDetailsSection/components/index.ts
@@ -1,11 +1,4 @@
 import WeatherDetailsSectionContent from "./WeatherDetailsSectionContent";
-import WeatherDetailsSectionError from "./WeatherDetailsSectionError";
-import WeatherDetailsSectionNoData from "./WeatherDetailsSectionNoData";
 import WeatherDetailsSectionSkeleton from "./WeatherDetailsSectionSkeleton";
 
-export {
-  WeatherDetailsSectionContent,
-  WeatherDetailsSectionError,
-  WeatherDetailsSectionNoData,
-  WeatherDetailsSectionSkeleton,
-};
+export { WeatherDetailsSectionContent, WeatherDetailsSectionSkeleton };
